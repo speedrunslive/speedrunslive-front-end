3146f15933fc740a028378e2fd704477b7a3fa88
dram55 <isbister1500@hotmail.com>
2017-02-23 10:52:15 -0500

    Add active button for races page

--- src/components/racesNavigation/index.js
+++ src/components/racesNavigation/index.js
@@ -11,12 +11,12 @@ const RacesNavigation = () => {
     <div className="racesNav">
       <div className="col-md-3">
         <h1><Translate content="racesNavigation.heading"/></h1>
-        <Link to="/races/live" className="btn btn-large red-hover"><Translate content="racesNavigation.liveRaces"/></Link>
-        <Link to="/races/seasons" className="btn btn-large red-hover"><Translate content="racesNavigation.seasons"/></Link>
-        <Link to="/races/racestats" className="btn btn-large red-hover"><Translate content="racesNavigation.raceStats"/></Link>
-        <Link to="/races/pastresults" className="btn btn-large red-hover"><Translate content="racesNavigation.pastResults"/></Link>
-        <Link to="/races/gamelist" className="btn btn-large red-hover"><Translate content="racesNavigation.gameList"/></Link>
-        <Link to="/races/bulletinboard" className="btn btn-large red-hover"><Translate content="racesNavigation.bulletinBoard"/></Link>
+        <Link to="/races/live" activeClassName="active-red" className="btn btn-large red-hover"><Translate content="racesNavigation.liveRaces"/></Link>
+        <Link to="/races/seasons" activeClassName="active-red" className="btn btn-large red-hover"><Translate content="racesNavigation.seasons"/></Link>
+        <Link to="/races/racestats" activeClassName="active-red" className="btn btn-large red-hover"><Translate content="racesNavigation.raceStats"/></Link>
+        <Link to="/races/pastresults" activeClassName="active-red" className="btn btn-large red-hover"><Translate content="racesNavigation.pastResults"/></Link>
+        <Link to="/races/gamelist" activeClassName="active-red" className="btn btn-large red-hover"><Translate content="racesNavigation.gameList"/></Link>
+        <Link to="/races/bulletinboard" activeClassName="active-red" className="btn btn-large red-hover"><Translate content="racesNavigation.bulletinBoard"/></Link>
       </div>
     </div>
   );
