87c947d7256bb3160d937797e10d5d840c5c9e09
dram55 <isbister1500@hotmail.com>
2017-02-23 09:30:40 -0500

    Add active class for main header navigation

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, IndexLink } from 'react-router';
 import Translate from 'react-translate-component';
 import counterpart from 'counterpart';
 import './navigation.scss';
@@ -10,11 +10,11 @@ const Navigation = () => {
   return (
     <nav>
       <div className="primary">
-        <Link to="/" className="btn blue-hover"><Translate content="navigation.streams"/></Link>
-        <Link to="/races" className="btn red-hover"><Translate content="navigation.races"/></Link>
-        <Link to="/channel" className="btn yellow-hover"><Translate content="navigation.channel"/></Link>
-        <Link to="/faq" className="btn green-hover"><Translate content="navigation.rules"/></Link>
-        <Link to="/tools" className="btn grey-hover"><Translate content="navigation.tools"/></Link>
+        <IndexLink to="/" activeClassName="active-blue" className="btn blue-hover"><Translate content="navigation.streams"/></IndexLink>
+        <Link to="/races" activeClassName="active-red" className="btn red-hover"><Translate content="navigation.races"/></Link>
+        <Link to="/channel" activeClassName="active-yellow" className="btn yellow-hover"><Translate content="navigation.channel"/></Link>
+        <Link to="/faq" activeClassName="active-green" className="btn green-hover"><Translate content="navigation.rules"/></Link>
+        <Link to="/tools" activeClassName="active-grey" className="btn grey-hover"><Translate content="navigation.tools"/></Link>
       </div>
     </nav>
   );
