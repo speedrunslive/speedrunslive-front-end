69c9d5bd4c614d195d165328cd7ed5103bcd6e4e
dram55 <isbister1500@hotmail.com>
2017-02-23 13:54:32 -0500

    Revert "Rules/FAQ button links to the rules page"
    This reverts commit c04f6f7af68dcf774b487ffaf41fcf3519795cb5.
    
    This change broke active button on header.

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -13,7 +13,7 @@ const Navigation = () => {
         <IndexLink to="/" activeClassName="active-blue" className="btn blue-hover"><Translate content="navigation.streams"/></IndexLink>
         <Link to="/races" activeClassName="active-red" className="btn red-hover"><Translate content="navigation.races"/></Link>
         <Link to="/channel" activeClassName="active-yellow" className="btn yellow-hover"><Translate content="navigation.channel"/></Link>
-        <Link to="/faq/rules" activeClassName="active-green" className="btn green-hover"><Translate content="navigation.rules"/></Link>
+        <Link to="/faq" activeClassName="active-green" className="btn green-hover"><Translate content="navigation.rules"/></Link>
         <Link to="/tools" activeClassName="active-grey" className="btn grey-hover"><Translate content="navigation.tools"/></Link>
       </div>
     </nav>
