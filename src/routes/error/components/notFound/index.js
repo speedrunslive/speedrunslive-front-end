73d9da651e899c2e0e5d0a70cefe9d62f64e31f8
dram55 <isbister1500@hotmail.com>
2017-03-05 19:54:35 -0500

    Add 404 page closes #92

---
+++ src/routes/error/components/notFound/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './notFound.scss';
+
+const NotFound = () => {
+  return (
+    <div className="container notFound">
+      <h1>404</h1>
+      <p>The page you were looking for was not found.</p>
+    </div>
+  );
+};
+export default NotFound;
