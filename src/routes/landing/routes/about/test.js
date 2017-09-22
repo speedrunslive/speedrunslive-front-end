f485250ffe89b233c3c37b6615afe9be46a9ac93
dram55 <isbister1500@hotmail.com>
2017-09-22 17:28:46 -0400

    Add About page unit tests

---
+++ src/routes/landing/routes/about/test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {expect} from 'chai';
+import {shallow} from 'enzyme';
+import About from './';
+
+describe('About', () =>{
+  it('renders', () => {
+    const component = shallow(<About />);
+    expect(component).to.exist;
+  });
+
+  it('renders children', () => {
+    const component = shallow(<About><div className='child'>a</div></About>);
+    expect(component.find('div.child').length).to.eq(1);
+  });
+});
