133457383c3c4b0e5aae2b260e73a79410aa0c4e
dram55 <isbister1500@hotmail.com>
2017-09-22 17:29:17 -0400

    Refactor About page, add new ops and remove static markup

--- src/routes/landing/routes/about/components/aboutBody/index.js
+++ src/routes/landing/routes/about/components/aboutBody/index.js
@@ -4,71 +4,41 @@ import {Link} from 'react-router';
 import './about.scss';
 
 const AboutBody = () => {
+  const ircOps = ["Garrison","Sumichu","Belthasar","freeball1"]
+  const ircHalfOps = ['bjw', 'Blechy', 'bony', 'CVagts', "JOPEBUSTER", 'mrcab', 'Prier', 'something915', 'ZaeloRae'];
+  const ircVoices = ["Acmlm", "bangerra", "ChristosOwen", "Countdown42", "CrescentEXE", "darkman78", "doicm", 
+  "Elminster", "fcoughlin", "feasel", "Genocidal", "Golden", "jkoper", "LiteYear", "Millnium", "mithical9", 
+  "neskamikaze", "prinny", "quo", "Simmons", "trogdor", "Twig", "usedpizza", "was0x", "Wawlconut", "Xelnas"];
+
+  function renderUser(user, role, displayName) {
+    return <p><Link to={`/profile/${user}`}>{(displayName)?displayName:user}</Link><span>{role}</span></p>
+  }
+
   return (
-    <div className="container aboutSRL">
+    <div className="about-body">
       <div className="col-md-7">
-        <div className="vertical-space"></div>
-        <p><Link to="/profile/Sluip">Sluip</Link><span className="dull">Lead operator, IRC & server admin</span></p>
-        <p><Link to="/profile/Blechy">Blechy</Link><span className="dull">Lead operator</span></p>
-        <p><Link to="/profile/Garrison">Garrison</Link><span className="dull">IRC op</span></p>
-        <p><Link to="/profile/sumichu">Sumichu</Link><span className="dull">IRC op</span></p>
-
+        {renderUser("Sluip", "Lead operator, IRC & server admin")}
+        {ircOps.map((user) => renderUser(user,"IRC Op"))}
         <hr/>
-
-        <p><Link to="/profile/Stauken">Stauken</Link><span className="dull">Teamleader for back-end development</span></p>
-        <p><Link to="/profile/esi">esi</Link><span className="dull">Developer</span></p>
-        <p><Link to="/profile/rainbowism">rainbowism</Link><span className="dull">Developer</span></p>
-
+        {renderUser("Stauken", "Teamleader for back-end development")}
+        {renderUser("rainbowism", "Developer")}
+        {renderUser("dram55", "Developer")}
         <hr/>
-
-        <p><Link to="/profile/bjw">bjw</Link><span className="dull">IRC halfop</span></p>
-        <p><Link to="/profile/bony">bony</Link><span className="dull">IRC halfop</span></p>
-        <p><Link to="/profile/CVagts">CVagts</Link><span className="dull">IRC halfop</span></p>
-        <p><Link to="/profile/Kirua">Kirua</Link><span className="dull">IRC halfop</span></p>
-        <p><Link to="/profile/Prier">Prier</Link><span className="dull">IRC halfop</span></p>
-        <p><Link to="/profile/something915">something915</Link><span className="dull">IRC halfop</span></p>
-
+        {ircHalfOps.map(user => renderUser(user, "IRC halfop"))}
         <hr/>
-
-        <p><Link to="/profile/Acmlm">Acmlm</Link><span className="dull">IRC voice</span></p>
-        <p><Link to="/profile/bangerra">bangerra</Link><span className="dull">IRC voice</span></p>
-        <p><Link to="/profile/Belthasar">Belthasar</Link><span className="dull">IRC voice</span></p>
-        <p><Link to="/profile/ChristosOwen">ChristosOwen</Link><span className="dull">IRC voice</span></p>
-        <p><Link to="/profile/crescentexe">CrescentEXE</Link><span className="dull">IRC voice</span></p>
-        <p><Link to="/profile/darkman78">darkman78</Link><span className="dull">IRC voice</span></p>
-        <p><Link to="/profile/doicm">doicm</Link><span className="dull">IRC voice</span></p>
-        <p><Link to="/profile/duneaught">duneaught</Link><span className="dull">IRC voice</span></p>
-        <p><Link to="/profile/Elminster">Elminster</Link><span className="dull">IRC voice</span></p>
-        <p><Link to="/profile/freeball1">freeball1</Link><span className="dull">IRC voice</span></p>
-        <p><Link to="/profile/Genocidal">Genocidal</Link><span className="dull">IRC voice</span></p>
-        <p><Link to="/profile/Golden">Golden</Link><span className="dull">IRC voice</span></p>
-        <p><Link to="/profile/LiteYear">LiteYear</Link><span className="dull">IRC voice</span></p>
-        <p><Link to="/profile/neskamikaze">neskamikaze</Link><span className="dull">IRC voice</span></p>
-        <p><Link to="/profile/prinny">prinny</Link><span className="dull">IRC voice</span></p>
-        <p><Link to="/profile/quo">quo</Link><span className="dull">IRC voice</span></p>
-        <p><Link to="/profile/Samura1man">Samura1man</Link><span className="dull">IRC voice</span></p>
-        <p><Link to="/profile/theevilisback">theevilisback</Link><span className="dull">IRC voice</span></p>
-        <p><Link to="/profile/Simmons">Simmons</Link><span className="dull">IRC voice</span></p>
-        <p><Link to="/profile/Trogdor">Trogdor</Link><span className="dull">IRC voice</span></p>
-        <p><Link to="/profile/usedpizza">usedpizza</Link><span className="dull">IRC voice</span></p>
-        <p><Link to="/profile/was0x">was0x</Link><span className="dull">IRC voice</span></p>
-        <p><Link to="/profile/Xelnas">Xelnas</Link><span className="dull">IRC voice</span></p>
-        <p><Link to="/profile/ZaeloRae">ZaeloRae</Link><span className="dull">IRC voice</span></p>
-
+        {ircVoices.map(user => renderUser(user, "IRC voice"))}
         <hr/>
-
-        <p><Link to="/profile/Mundungu">Mundungu</Link><span className="dull">SRL Seasons front-end code</span></p>
-        <p><Link to="/profile/IvanGS">IvanGS</Link><span className="dull">Back-end development</span></p>
-        <p><Link to="/profile/joe">joe</Link><span className="dull">Coded JOPEBUSTER, bugfixed RaceBot</span></p>
-        <p><Link to="/profile/izu">izu</Link><span className="dull">Japanese translation</span></p>
-
+        {renderUser("Mundungu", "SRL Seasons front-end code")}
+        {renderUser("IvanGS", "Back-end development")}
+        {renderUser("joe", "Coded JOPEBUSTER, bugfixed RaceBot")}
+        {renderUser("izu", "Japanese translation")}
+        {renderUser("esi", "Developer")}
         <hr/>
-
-        <p><Link to="/profile/Narcissa">Narcissa Wright</Link><span className="dull">Co-founder, designer, helped code front-end</span></p>
-        <p><Link to="/profile/bmn">bmn</Link><span className="dull">Helped code the original streams page</span></p>
-        <p><Link to="/profile/TRV">TRV</Link><span className="dull">Helped design the original database</span></p>
-        <p><Link to="/profile/Jiano">Daniel "Jiano" Hart</Link><span className="dull">Co-founder, coded RaceBot and front-end site</span></p>
-        <p><Link to="/profile/puchiedarcy">Zach "puchiedarcy" Layman</Link><span className="dull">Server admin, developer of the SRL API</span></p>
+        {renderUser("Narcissa", "Co-founder, designer, helped code front-end", "Narcissa Wright")}
+        {renderUser("bmn", "Helped code the original streams page")}
+        {renderUser("TRV", "Helped design the original database")}
+        {renderUser("Jiano", "Co-founder, coded RaceBot and front-end site", 'Daniel "Jiano" Hart')}
+        {renderUser("puchiedarcy", "Developer of the SRL API" ,'Zach "puchiedarcy" Layman')}
       </div>
     </div>
   );
