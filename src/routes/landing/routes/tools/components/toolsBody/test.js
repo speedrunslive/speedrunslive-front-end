35f26cbe93bc714a0563e0f7e158686ff549d3f6
dram55 <isbister1500@hotmail.com>
2017-09-21 23:20:07 -0400

    Add unit tests to Tools

---
+++ src/routes/landing/routes/tools/components/toolsBody/test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {expect} from 'chai';
+import {shallow} from 'enzyme';
+import ToolsBody from '../toolsBody';
+
+describe('ToolsBody', () =>{
+  it('renders', () => {
+    const component = shallow(<ToolsBody />);
+    expect(component).to.exist;
+  });
+});
