c679e2a786e8220427606a7c3573ccecdc767c3f
dram55 <isbister1500@hotmail.com>
2017-09-21 23:13:57 -0400

    Add Promotion unit tests

---
+++ src/routes/landing/routes/promotion/components/promoBody/test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {expect} from 'chai';
+import {shallow} from 'enzyme';
+import PromoBody from '../promoBody';
+
+describe('PromoBody', () =>{
+  it('renders', () => {
+    const component = shallow(<PromoBody />);
+    expect(component).to.exist;
+  });
+});
---
+++ src/routes/landing/routes/promotion/test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {expect} from 'chai';
+import {shallow} from 'enzyme';
+import Promotion from '../promotion';
+
+describe('Promotion Route Handler', () =>{
+  it('renders', () => {
+    const component = shallow(<Promotion />);
+    expect(component).to.exist;
+  });
+
+  it('renders children when passed', () => {
+    const component = shallow(<Promotion><div className='child'>a</div></Promotion>);
+    expect(component.find('div.child').length).to.eq(1);
+  })
+});
