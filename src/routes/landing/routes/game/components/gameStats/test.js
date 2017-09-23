3f84323a8716cd178f31c960e3b1424e43f6d7e8
dram55 <isbister1500@hotmail.com>
2017-09-22 20:57:55 -0400

    Add unit test for game stats

---
+++ src/routes/landing/routes/game/components/gameStats/test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {expect} from 'chai';
+import {shallow} from 'enzyme';
+import GameStats from './';
+
+describe('GameStats', () =>{
+  it('renders', () => {
+    const component = shallow(<GameStats />);
+    expect(component).to.exist;
+  });
+
+  it('will display correct stats', () => {
+    const componentText = shallow(<GameStats game={game} stats={stats} />).text();
+    expect(componentText).to.contain('Abbreviation: ' + game.abbrev);
+    expect(componentText).to.contain('Races: ' + stats.totalRaces);
+  });
+});
+
+const game = {id: 326, name: "The Magical Quest Starring Mickey Mouse", abbrev: "mmq", popularity: 36, popularityrank: 27};
+const stats = {totalRaces: 92, totalPlayers: 79, largestRace: 7792, largestRaceSize: 7, totalRaceTime: 222144};
