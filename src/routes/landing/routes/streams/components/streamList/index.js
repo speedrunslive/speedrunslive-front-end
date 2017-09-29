5a3b22255f3795075bfe46bbf032a291dbbac528
dram55 <isbister1500@hotmail.com>
2017-09-29 15:58:39 -0400

    Add StreamList unit tests

--- src/routes/landing/routes/streams/components/streamList/index.js
+++ src/routes/landing/routes/streams/components/streamList/index.js
@@ -3,16 +3,22 @@ import StreamListItem from '../streamListItem';
 
 import './streamList.scss';
 
-function buildStreamList(stream, onClick){
-  return <StreamListItem key={stream.name} onClick={onClick} stream={stream} />;
-}
-
+/**
+ * StreamList - build list of <StreamListItem />
+ * 
+ * @param {function} onClick - callback for parent to handle when a particular child is clicked
+ * @param {Array} streams - list of stream objects, to build <StreamListItem /> from each
+ */
 const StreamList = (props) => {
   return (
     <div className="stream-list">
         {props.streams.map((stream)=>{return buildStreamList(stream, props.onClick)})}
     </div>
   );
+
+  function buildStreamList(stream, onClick){
+    return <StreamListItem key={stream.name} onClick={onClick} stream={stream} />;
+  }
 };
 
 export default StreamList;
