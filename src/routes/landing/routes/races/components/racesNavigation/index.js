aa509a2ee81fe2179aaba1a782057118c941e481
dram55 <isbister1500@hotmail.com>
2017-10-06 00:22:53 -0400

    Added Race Icons, using cdn Closes #22

--- src/routes/landing/routes/races/components/racesNavigation/index.js
+++ src/routes/landing/routes/races/components/racesNavigation/index.js
@@ -10,16 +10,32 @@ const RacesNavigation = () => {
   return (
     <div className="racesNav">
       <div className="col-md-3">
-        <h1><Translate content="racesNavigation.heading"/></h1>
-        <Link to="/races/live" activeClassName="active-red" className="btn btn-large red-hover"><Translate content="racesNavigation.liveRaces"/></Link>
-        <Link to="/races/seasons" activeClassName="active-red" className="btn btn-large red-hover"><Translate content="racesNavigation.seasons"/></Link>
-        <Link to="/races/racestats" activeClassName="active-red" className="btn btn-large red-hover"><Translate content="racesNavigation.raceStats"/></Link>
-        <Link to="/races/pastresults" activeClassName="active-red" className="btn btn-large red-hover"><Translate content="racesNavigation.pastResults"/></Link>
-        <Link to="/races/gamelist" activeClassName="active-red" className="btn btn-large red-hover"><Translate content="racesNavigation.gameList"/></Link>
-        <Link to="/races/bulletinboard" activeClassName="active-red" className="btn btn-large red-hover"><Translate content="racesNavigation.bulletinBoard"/></Link>
+        <h1><Translate content="racesNavigation.heading" /></h1>
+        <Link to="/races/live" activeClassName="active-red" className="btn btn-large red-hover">
+          <Translate content="racesNavigation.liveRaces" />
+          <img src="http://cdn.speedrunslive.com/images/liveraces.png" alt="Live Races" />
+        </Link>
+        <Link to="/races/seasons" activeClassName="active-red" className="btn btn-large red-hover">
+          <Translate content="racesNavigation.seasons" />
+          <img src="http://cdn.speedrunslive.com/images/seasons.png" alt="Seasons" />
+        </Link>
+        <Link to="/races/racestats" activeClassName="active-red" className="btn btn-large red-hover">
+          <Translate content="racesNavigation.raceStats" /><img src="http://cdn.speedrunslive.com/images/pastresults.png" alt="Past Results" />
+        </Link>
+        <Link to="/races/pastresults" activeClassName="active-red" className="btn btn-large red-hover">
+          <Translate content="racesNavigation.pastResults" /><img src="http://cdn.speedrunslive.com/images/awards.png" alt="Awards" />
+        </Link>
+        <Link to="/races/gamelist" activeClassName="active-red" className="btn btn-large red-hover">
+          <Translate content="racesNavigation.gameList" /><img src="http://cdn.speedrunslive.com/images/gamelist.png" alt="Game List" />
+        </Link>
+        <Link to="/races/bulletinboard" activeClassName="active-red" className="btn btn-large red-hover">
+          <Translate content="racesNavigation.bulletinBoard" /><img src="http://cdn.speedrunslive.com/images/bboard.png" alt="Board" />
+        </Link>
       </div>
     </div>
   );
 }
 
 export default RacesNavigation;
+
+// 
